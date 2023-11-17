03e97b78576e8dd6107c778954f5185eb7c868cb
Anith Thankachen <anithtt@gmail.com>
2023-11-17 12:18:26 +0000

    Initial changes - 1. Created the project . 2. Made it more manageable following the clean architecture pattern

---
+++ WeatherForecast/WeatherForecast.Core/Entity/WeatherInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherForecast.Core.Entity
+{
+    public class WeatherInfo
+    {
+        public string LocationName { get; set; }
+        public double Temperature { get; set; }
+        public int Humidity { get; set; }
+        public DateTime Sunrise { get; set; }
+        public DateTime Sunset { get; set; }
+    }
+}
